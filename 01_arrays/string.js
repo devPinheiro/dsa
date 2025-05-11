106910cd37dc27296d33f2b81edac457ac1a4ba0
devPinheiro <pinheirolaoluwa@gmail.com>
2025-05-11 23:01:30 +0100

    arrays

--- 01_arrays/string.js
+++ 01_arrays/string.js
@@ -82,6 +82,8 @@ const reverser3 = (str) => [...str].reverse().join('')
 
 
 
+
+
 /**
  * 
  * Palindrome
@@ -92,5 +94,25 @@ const reverser3 = (str) => [...str].reverse().join('')
 
 /**
  * 
- * 
+ * Given a string str consisting of characters such as '(', ')', '{', '}', '[' and ']', determine if the input string is properly balanced.
+
+A string is considered balanced if:
+
+Each opening bracket is closed by the same type of bracket (e.g., ( with ), { with }, and [ with ])
+Open brackets are closed in the correct order (e.g., ([]) is valid, but ([)] is not)
+Any subset of brackets enclosed within a matched pair must also form a valid matched pair (e.g., {[(])} is not balanced because the contents inside { and } are unbalanced).
+
+Input
+str: string: A string
+Examples
+
+Input: str = "[]"
+Output: true
+Explanation: The string contains correctly paired and ordered parentheses.
+Input: str = "([)]"
+Output: false
+Explanation: The string contains correctly paired but incorrectly ordered parentheses.
+Input: str = "([]){}"
+Output: true
+Explanation: The string contains correctly paired and ordered parentheses
  */
